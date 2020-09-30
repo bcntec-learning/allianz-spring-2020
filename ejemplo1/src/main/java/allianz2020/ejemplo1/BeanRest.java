73d19b0bfa3c274cc308b8b4e957001028dbfa4c
Francsico Philip <fphilip@bcntec.com>
2020-09-30 14:13:59 +0200

    header

--- ejemplo1/src/main/java/allianz2020/ejemplo1/BeanRest.java
+++ ejemplo1/src/main/java/allianz2020/ejemplo1/BeanRest.java
@@ -1,11 +1,13 @@
 package allianz2020.ejemplo1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -24,26 +26,27 @@ public class BeanRest {
         return beanService.getTax();
     }
 
-    @GetMapping(value="/calc", params = "value")
-    public BigDecimal sum(@RequestParam BigDecimal value, HttpSession session) {
+    @GetMapping(value = "/calc", params = "value")
+    public BigDecimal sum(@RequestParam BigDecimal value, @RequestHeader("Accept-Language") String lang,  HttpSession session) {
         BigDecimal total = Optional.ofNullable((BigDecimal) session.getAttribute("TOTAL")).orElse(BigDecimal.ZERO);
         List<BigDecimal> numbers = (List<BigDecimal>) Optional.ofNullable(session.getAttribute("DATA"))
-                .orElse(Collections.emptyList());
+                .orElse(new ArrayList<>());
         numbers.add(value);
-        BigDecimal u =  beanService.calc(value);
-        total= total.add(u);
+        BigDecimal u = beanService.calc(value);
+        total = total.add(u);
         session.setAttribute("TOTAL", total);
         session.setAttribute("DATA", numbers);
+        System.err.println(lang);
         return total;
     }
 
-    @GetMapping(value="/reset")
-    public void sum( HttpSession session) {
+    @GetMapping(value = "/reset")
+    public void sum(HttpSession session) {
         Optional.ofNullable(session).ifPresent(HttpSession::invalidate);
     }
 
     @GetMapping("/add")
     public BigDecimal sum(@RequestParam BigDecimal value) {
-        return  beanService.add(value);
+        return beanService.add(value);
     }
 }
--- ejemplo1/src/main/java/allianz2020/ejemplo1/Ejemplo1Application.java
+++ ejemplo1/src/main/java/allianz2020/ejemplo1/Ejemplo1Application.java
@@ -3,6 +3,7 @@ package allianz2020.ejemplo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
